46ff7a5e02fe100845b8d2ae8738f7eca76898c8
Benjamin Reiner <benjamin.t.reiner@gmail.com>
2023-09-22 12:33:26 -0400

    Add basic colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,44 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          "50": "#effce9",
+          "100": "#dbf7d0",
+          "200": "#baf1a5",
+          "300": "#8fe571",
+          "400": "#69d645",
+          "500": "#42a923",
+          "600": "#35951b",
+          "700": "#2b7219",
+          "800": "#255b19",
+          "900": "#234d1a",
+          "950": "#0e2a09",
+        },
+        neutral: {
+          "50": "#f5f5f6",
+          "100": "#e6e6e7",
+          "200": "#cfcfd2",
+          "300": "#aeaeb2",
+          "400": "#86878a",
+          "500": "#6b6c70",
+          "600": "#5b5b5f",
+          "700": "#4d4d51",
+          "800": "#444546",
+          "900": "#3c3c3d",
+          "950": "#252527",
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
