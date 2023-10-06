2dcba8ff2a9b5ebd27a846efb50833385014f79e
Benjamin Reiner <benjamin.t.reiner@gmail.com>
2023-10-06 13:14:50 -0400

    Add submission and error logic

--- utils/components/ContactForm.tsx
+++ utils/components/ContactForm.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { twMerge } from "tailwind-merge";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { useReCaptcha } from "next-recaptcha-v3";
+import { type SubmitHandler, useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Button from "./Buttons/Button";
@@ -39,80 +40,138 @@ function ContactForm({ className }: ContactFormProps) {
     handleSubmit,
     formState: { errors, isSubmitting },
   } = useForm<SchemaType>({ mode: "onSubmit", resolver: zodResolver(schema) });
+  const { executeRecaptcha } = useReCaptcha();
+  const [submissionError, setSubmissionError] = useState<SubmissionError>({
+    tripped: false,
+    message: "",
+  });
+  const [submitted, setSubmitted] = useState<boolean>(false);
 
-  const onSubmit: SubmitHandler<FormData> = (data: FormData) => {
-    console.log(data);
+  const onSubmit: SubmitHandler<FormData> = async (data: FormData) => {
+    setSubmissionError({ tripped: false, message: "" });
+
+    // Check if executeRecaptcha is initialized
+    if (!executeRecaptcha) {
+      setSubmissionError({
+        tripped: true,
+        message: "reCaptcha not loaded",
+      });
+      return;
+    }
+
+    // Get reCaptcha token
+    const token = await executeRecaptcha("onSubmit");
+
+    // Submit form data
+    const formSubmission = { data, token };
+    try {
+      const response = await fetch("/api/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formSubmission),
+      }).then((res) => res.json());
+
+      response.success
+        ? setSubmitted(true)
+        : setSubmissionError({
+            tripped: true,
+            message: response.message,
+          });
+    } catch (e) {
+      if (typeof e === "string") {
+        console.error(e);
+        setSubmissionError({ tripped: true, message: e });
+      } else if (e instanceof Error) {
+        console.error(e.message);
+        setSubmissionError({ tripped: true, message: e.message });
+      }
+    }
   };
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className={twMerge("grid gap-4 md:grid-cols-2", className)}
-    >
-      <div>
-        <label htmlFor="name" className="sr-only">
-          Name
-        </label>
-        <input
-          type="text"
-          id="name"
-          placeholder="Name"
-          className="w-full px-4 py-2"
-          {...register("name")}
-        />
-      </div>
-      <div>
-        <label htmlFor="email" className="sr-only">
-          Email Address
-        </label>
-        <input
-          type="text"
-          id="email"
-          placeholder="Email Address"
-          className="w-full px-4 py-2"
-          {...register("email")}
-        />
-      </div>
-      <div className="col-span-full">
-        <label htmlFor="message" className="sr-only">
-          Message
-        </label>
-        <textarea
-          id="message"
-          placeholder="Message"
-          className="h-36 w-full px-4 py-2"
-          {...register("message")}
-        />
-      </div>
-      <div className="col-span-full flex flex-col gap-2">
-        <Button
-          type="submit"
-          disabled={isSubmitting}
-          className="w-full font-semibold md:w-fit md:self-end"
+    <section>
+      {submitted ? (
+        <p>
+          Thank you for contacting us! We&apos;ll get back to you as soon as
+          possible.
+        </p>
+      ) : (
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className={twMerge("grid gap-4 md:grid-cols-2", className)}
         >
-          Submit
-        </Button>
-        {(errors.name || errors.email || errors.message) && (
-          <ul className="flex-col self-center text-center text-red-500 md:self-end md:text-end">
-            {errors.name && (
-              <li>
-                <label htmlFor="name">{errors.name.message}</label>
-              </li>
+          <div>
+            <label htmlFor="name" className="sr-only">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              placeholder="Name"
+              className="w-full px-4 py-2"
+              {...register("name")}
+            />
+          </div>
+          <div>
+            <label htmlFor="email" className="sr-only">
+              Email Address
+            </label>
+            <input
+              type="text"
+              id="email"
+              placeholder="Email Address"
+              className="w-full px-4 py-2"
+              {...register("email")}
+            />
+          </div>
+          <div className="col-span-full">
+            <label htmlFor="message" className="sr-only">
+              Message
+            </label>
+            <textarea
+              id="message"
+              placeholder="Message"
+              className="h-36 w-full px-4 py-2"
+              {...register("message")}
+            />
+          </div>
+          <div className="col-span-full flex flex-col gap-2">
+            <Button
+              type="submit"
+              disabled={isSubmitting}
+              className="w-full font-semibold md:w-fit md:self-end"
+            >
+              Submit
+            </Button>
+            {(errors.name ||
+              errors.email ||
+              errors.message ||
+              submissionError.tripped) && (
+              <ul className="flex-col self-center text-center text-red-500 md:self-end md:text-end">
+                {errors.name && (
+                  <li>
+                    <label htmlFor="name">{errors.name.message}</label>
+                  </li>
+                )}
+                {errors.email && (
+                  <li>
+                    <label htmlFor="email">{errors.email.message}</label>
+                  </li>
+                )}
+                {errors.message && (
+                  <li>
+                    <label htmlFor="message">{errors.message.message}</label>
+                  </li>
+                )}
+                {submissionError.tripped && <li>{submissionError.message}</li>}
+              </ul>
             )}
-            {errors.email && (
-              <li>
-                <label htmlFor="email">{errors.email.message}</label>
-              </li>
-            )}
-            {errors.message && (
-              <li>
-                <label htmlFor="message">{errors.message.message}</label>
-              </li>
-            )}
-          </ul>
-        )}
-      </div>
-    </form>
+          </div>
+        </form>
+      )}
+    </section>
   );
 }
 
